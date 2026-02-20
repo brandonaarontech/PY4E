f105b5c97dfdb8b797cdd900b2f8528dae900b8f
Brandon Aaron Bustamante <contact@brandonaaron.tech>
2026-02-19 23:21:24 -0700

    CH_08 done

--- CH_08_Lists/ex_08_05.py
+++ CH_08_Lists/ex_08_05.py
@@ -54,8 +54,8 @@ count = 0
 
 for line in file:
     if line.startswith("From "):
-        word = line.split()
-        print(word[1])
+        words = line.split()
+        print(words[1])
         count += 1
 
 print("There were", count, "lines in the file with From as the first word")
